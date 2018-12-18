c187e1bb8b30729c8c91267811a66ba669a6eae4
Gautam Malu <gautam.malu@snap2insight.com>
2018-12-18 05:28:20 +0000

    Added removal of .gitkeep files

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+find ./ -name ".gitke*" -exec rm -rf {} \;
 func()
 {
 	echo -n "Continue If All Missed UPC's have been added to the list(y/n):"
@@ -26,7 +27,7 @@ done
 
 # Arguments required  (--split_size n n, --gt 1(1 for generate groundtruth files,  0 for not), --aug 1 (implement augmentations in split images, 1 for yes) , --split_option 1 ( for splitting the image)
 
-time python rcnn_train.py --split_size 1 1 --gt 0 --aug 0 --split 0
+time python rcnn_train.py --split_size 4 4 --gt 1 --aug 1 --split 1
 
 
 #creating symlinks to dataset
